5a01059a97958a7ff9898bb08afc4174a0309b9c
mayankrb <98233030+mayankrb@users.noreply.github.com>
2022-11-27 17:11:06 -0500

    Added data-coupons, employees, products & sellers

---
+++ data insertion into tables sql files/Creating Part tables and entering data.sql
@@ -0,0 +1,157 @@
+INSERT INTO coupons (coupon_id , name , discount , start_date , end_date)
+values ('CBAYCOUPON1' ,  'FLAT20' ,  0.2 ,  '2020-01-01' ,  '2030-01-01' );
+INSERT INTO coupons (coupon_id , name , discount , start_date , end_date)
+values ('CBAYCOUPON2' ,  'BLACKFRIDAY' ,  0.5 ,  '2021-11-26' ,  '2021-11-26' );
+INSERT INTO coupons (coupon_id , name , discount , start_date , end_date)
+values ('CBAYCOUPON3' ,  'FIRSTORDER' ,  0.5 ,  '2020-01-01' ,  '2030-01-01' );
+INSERT INTO coupons (coupon_id , name , discount , start_date , end_date)
+values ('CBAYCOUPON4' ,  'SUMMERSALE' ,  0.3 ,  '2021-05-01' ,  '2021-09-01' );
+INSERT INTO coupons (coupon_id , name , discount , start_date , end_date)
+values ('CBAYCOUPON5' ,  'WINTERSALE' ,  0.3 ,  '2021-09-01' ,  '2022-03-01' );
+
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP00001','Nicola Oliver','Worker','receiving');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP00002','Jason Rees','Worker','receiving');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP00003','Natalie Jones','Worker','receiving');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP00004','Harry Avery','Worker','receiving');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP00005','Adrian Paige','Worker','putaway');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP00006','Cameron King','Worker','putaway');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP00007','Joe Forsyth','Worker','putaway');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP00008','Penelope Ellison','Worker','putaway');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP00009','Sarah Edmunds','Worker','storage');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000010','Jane Jackson','Worker','storage');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000011','Mary Mitchell','Worker','storage');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000012','Alexandra Langdon','Worker','storage');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000013','Diane Edmunds','Worker','picking');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000014','Peter Welch','Worker','picking');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000015','Jake Roberts','Worker','picking');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000016','Brian Hughes','Worker','picking');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000017','Katherine Sanderson','Worker','packing');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000018','Ryan Peake','Worker','packing');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000019','Frank Parsons','Worker','packing');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000020','Stephen Powell','Worker','packing');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000021','Rose Sanderson','Worker','shipping');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000022','Luke Bower','Worker','shipping');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000023','Jack Henderson','Worker','shipping');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000024','Chloe Lyman','Worker','shipping');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000025','Abigail Lee','SuperVisor','Supervision');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000026','Zoe Howard','SuperVisor','Supervision');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000027','Virginia Reid','SuperVisor','Supervision');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000028','Nathan King','SuperVisor','Supervision');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000029','David Henderson','Manager','Management');
+INSERT INTO Employees (employee_id, employee_name, designation, department)
+values ('CBAYEMP000030','Blake Metcalfe','Manager','Management');
+
+
+
+
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT0001','C-bay Mattress',  149.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT0002','C-bay Pillows',  14.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT0003','C-bay Cloth Hangars',  5.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT0004','C-bay Bed Frame',  74.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT0005','C-bay Bed Sheet',  13.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT0006','C-bay Comforter',  24.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT0007','C-bay Wine Glasses',  27.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT0008','C-bay Plate Set',  34.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT0009','C-bay Storage Container Set',  19.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00010','C-bay Knife Set',  16.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00011','C-bay Spoon Set',  9.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00012','C-bay Cast Iron Skillet',  13.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00013','C-bay Heater',  29.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00014','C-bay Air Conditioner',  174.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00015','C-bay Fan',  14.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00016','C-bay Kitchen Cart',  12.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00017','C-bay Coffee Table',  134.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00018','C-bay Body Wash',  5.23 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00019','C-bay Shampoo and Conditioner',  4.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00020','C-bay Dry Bath Towel',  14.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00021','C-bay Curtain Rod',  14.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00022','C-bay Handwash',  2.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00023','C-bay Toilet Roll Pack',  14.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00024','C-bay Trash Bags 40 Count',  19.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00025','C-bay Laundry Detergent Pacs',  16.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00026','C-bay Storage Bags 300 count',  18.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00027','C-bay Razor',  14.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00028','C-bay Mop',  16.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00029','C-bay Paper Towels',  20.99 ,  1000 );
+INSERT INTO Products (product_id, name, price, availability)
+values ('CBAYPDT00030','C-bay Handsoap Liquid',  9.99 ,  1000 );
+
+
+INSERT INTO Sellers (seller_id, name, email, address, phone_num)
+values ('CBAYSELLER1','Wendy Barajas','wendy.barajas@outlook.com','738 East Country Dr. Traverse City, MI 49684','(606) 277-3789');
+INSERT INTO Sellers (seller_id, name, email, address, phone_num)
+values ('CBAYSELLER2','Kason Webber','kason.webber@yahoo.com','42 North Pineknoll Ave. Mchenry, IL 60050','(753) 202-3905');
+INSERT INTO Sellers (seller_id, name, email, address, phone_num)
+values ('CBAYSELLER3','Siobhan Brooks','siobhan.brooks@outlook.com','4 Johnson Ave. Saint Albans, NY 11412','(444) 793-9896');
+INSERT INTO Sellers (seller_id, name, email, address, phone_num)
+values ('CBAYSELLER4','Griff Noble','griff.noble@outlook.com','526 East Greenview Rd. Kenosha, WI 53140','(967) 383-1737');
+INSERT INTO Sellers (seller_id, name, email, address, phone_num)
+values ('CBAYSELLER5','Hania Dillard','hania.dillard@gmail.com','985 Old 2nd Drive Gibsonia, PA 15044','(391) 627-0383');
+INSERT INTO Sellers (seller_id, name, email, address, phone_num)
+values ('CBAYSELLER6','Rares Gordon','rares.gordon@gmail.com','513 Birchwood Drive Mount Airy, MD 21771','(444) 883-8518');
+INSERT INTO Sellers (seller_id, name, email, address, phone_num)
+values ('CBAYSELLER7','Maryam Aguirre','maryam.aguirre@outlook.com','974 Devon Rd. Waltham, MA 02453','(689) 752-2005');
+INSERT INTO Sellers (seller_id, name, email, address, phone_num)
+values ('CBAYSELLER8','Riley Dodson','riley.dodson@gmail.com','851 Harvey Street Tuckerton, NJ 08087','(911) 896-3287');
+INSERT INTO Sellers (seller_id, name, email, address, phone_num)
+values ('CBAYSELLER9','Myra Mcgill','myra.mcgill@gmail.com','8760 Saxton Street Maplewood, NJ 07040','(835) 554-4090');
+INSERT INTO Sellers (seller_id, name, email, address, phone_num)
+values ('CBAYSELLER10','Skyla Kennedy','skyla.kennedy@yahoo.com','8935 Sage Avenue Covington, GA 30014','(918) 820-1393');
