4f089aed5886bc9f048c2c62e842d4777b038611
unknown <mb7992@nyu.edu>
2022-12-14 15:09:08 -0500

    updated pro.sql

--- pro_schema.sql
+++ pro_schema.sql
@@ -16,5 +16,33 @@ Create table HasCards(
 	Foreign key(customer_id) references Customers(customer_id)
 );
 
+Create table Sellers(
+    seller_id varchar(16) primary key,
+    name varchar(50) not null,
+    email varchar(50) not null,
+    address varchar(256) not null,
+    phone_num varchar(16) not null
+);
 
+Create table Employees(
+    employee_id varchar(16) primary key,
+    employee_name varchar(50) not null,
+    designation varchar(50) not null,
+    department varchar(50) not null
+);
 
+Creating products data
+Create table Products(
+    product_id varchar(16) primary key,
+    name varchar(50) not null,
+    price real not null,
+    availability integer not null
+);
+
+create table Coupons(
+    coupon_id varchar(16) primary key,
+    name varchar(16) not null,
+    discount real not null,
+    start_date date not null, - yyyy-mm-dd
+    end_date date not null - yyyy-mm-dd
+);
